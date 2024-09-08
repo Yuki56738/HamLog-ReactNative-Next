e309f2c435029cb7585d6cdd1b95f877f2eee776
Yuki <github@risaton.net>
2024-09-09 04:40:11 +0900

    init backend.

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	_ "github.com/labstack/echo/v4"
+	_ "github.com/labstack/echo/v4/middleware"
+)
 
+func main() {
+	fmt.Println("running api server...")
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
