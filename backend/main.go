4f8e4029ec1d9eb627b6f97526ea2067f9640416
Yuki <github@risaton.net>
2024-09-09 08:01:03 +0900

    open db

--- backend/main.go
+++ backend/main.go
@@ -24,22 +24,23 @@ func main() {
 
 }
 
-type hamlog struct {
-	ID        int    `gorm:"primaryKey"`
-	CreatedAt string `gorm:"type:datetime"`
-	UpdatedAt string `gorm:"type:datetime"`
-	CALLSIGN  string
-	HISRS     int
-	MYRS      int
-	QTH       string
-	NAME      string
-	REMARK    string
+type hamlogs struct {
+	ID int `gorm:"primaryKey"`
+	//CreatedAt string `gorm:"type:datetime"`
+	//UpdatedAt string `gorm:"type:datetime"`
+	CALLSIGN string
+	HISRS    int
+	MYRS     int
+	QTH      string
+	NAME     string
+	REMARK   string
 }
 
 func handleDb() {
 	g := gen.NewGenerator(gen.Config{
-		OutPath: "./query",
-		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface,
+		OutPath:       "./query",
+		Mode:          gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface,
+		FieldNullable: true,
 	})
 	var dbToCreateDb *gorm.DB
 	dbToCreateDb, _ = gorm.Open(mysql.Open("root:password@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
@@ -49,6 +50,15 @@ func handleDb() {
 	var db *gorm.DB
 	db, _ = gorm.Open(mysql.Open("root:password@tcp(127.0.0.1:3306)/hamlog?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	g.UseDB(db)
+	//g.Execute()
+	//test := hamlogs{
+	//	CALLSIGN: "test",
+	//	HISRS:    1,
+	//	MYRS:     1,
+	//	QTH:      "test",
+	//	NAME:     "test",
+	//	REMARK:   "test",
+	//}
+	//db.Create(&test)
 	g.Execute()
-
 }
