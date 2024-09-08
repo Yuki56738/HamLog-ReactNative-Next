fe97725a07022ae4daef6dc76786b80989b1257c
Yuki <github@risaton.net>
2024-09-09 04:15:08 +0900

    commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import {Image, StyleSheet, Platform, ScrollView, Text, View, TextInput} from 'react-native';
 
 
 
 export default function HomeScreen() {
-  return (
+    return (
         <ScrollView style={styles.container}>
             <Text>Some text</Text>
             <TextInput
+                style={styles.input}
                 placeholder='Type here'
                 ></TextInput>
+
         </ScrollView>
   );
 }
@@ -20,5 +23,12 @@ const styles = StyleSheet.create({
     paddingTop: 70,
       backgroundColor: 'white',
       height: 40
-  }
+  },
+    input: {
+        // height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        // padding: 10,
+        borderRadius: 5 // 追加で丸い縁取り効果を適用することができます
+    }
 });
