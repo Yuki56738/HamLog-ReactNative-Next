465c1dafd14c9f3c159c4cbd8c99d28df2993775
Yuki <github@risaton.net>
2024-09-09 08:37:31 +0900

    commit

--- backend/main.go
+++ backend/main.go
@@ -6,11 +6,12 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gen"
 	"gorm.io/gorm"
+	"time"
 )
 
 func main() {
 	fmt.Println("running api server...")
-	handleDb()
+	//handleDb("QZ1W", 10, 10, "QZ1W", "test", "test")
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
@@ -20,13 +21,17 @@ func main() {
 	e.POST("ping", func(c echo.Context) error {
 		return c.String(200, "Pong!")
 	})
+	e.POST("add", func(c echo.Context) error {
+
+		return c.String(200, "OK")
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 
 }
 
 type hamlogs struct {
-	ID int `gorm:"primaryKey"`
-	//CreatedAt string `gorm:"type:datetime"`
+	ID        int `gorm:"primaryKey"`
+	CreatedAt string
 	//UpdatedAt string `gorm:"type:datetime"`
 	CALLSIGN string
 	HISRS    int
@@ -36,7 +41,7 @@ type hamlogs struct {
 	REMARK   string
 }
 
-func handleDb() {
+func handleDb(callsign string, hisrs int, myrs int, qth string, name string, remark string) {
 	g := gen.NewGenerator(gen.Config{
 		OutPath:       "./query",
 		Mode:          gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface,
@@ -50,15 +55,30 @@ func handleDb() {
 	var db *gorm.DB
 	db, _ = gorm.Open(mysql.Open("root:password@tcp(127.0.0.1:3306)/hamlog?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	g.UseDB(db)
+	g.Execute()
+
+	log := hamlogs{
+		CALLSIGN:  callsign,
+		HISRS:     hisrs,
+		MYRS:      myrs,
+		QTH:       qth,
+		NAME:      name,
+		REMARK:    remark,
+		CreatedAt: time.Now().String(),
+	}
+	err := db.AutoMigrate(&hamlogs{})
+	if err != nil {
+		return
+	}
 	//g.Execute()
-	//test := hamlogs{
-	//	CALLSIGN: "test",
-	//	HISRS:    1,
-	//	MYRS:     1,
-	//	QTH:      "test",
-	//	NAME:     "test",
-	//	REMARK:   "test",
-	//}
-	//db.Create(&test)
+	//db.Create(&log)
+	g.Execute()
+	err = db.Transaction(func(tx *gorm.DB) error {
+		err := tx.Create(&log).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 	g.Execute()
 }
